4e8a2556d7c4e72ebb9a61679d8cfa873cc8efa8
Simon <mocki.toki@gmail.com>
2024-03-02 18:30:03 +0300

    Initial Commit

---
+++ Pictorium/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pictorium
+//
+//  Created by Simon Butenko on 02.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
